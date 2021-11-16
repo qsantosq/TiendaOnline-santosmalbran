5556e888d54b60aba29946678a61ef96b1a050ab
Santos Malbran <malbran.santos@gmail.com>
2021-11-16 18:40:53 -0300

    second commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+
+            <header className="contenedor-header">
+
+                <div className="contenedor-logo">
+                <img src={require("../assest/logo.png").default} className="logo" />
+                </div>
+
+                <div className="contenedor-incio">
+                   <button className="botones-menu">Inicio</button>
+                </div>
+
+                <div className="contenedor-navbar">
+                    <nav>
+                        <ul className="contenedor-ul">
+                            <li><a href="#" className="estilos-a">Hamburguesas</a></li>
+                            <li><a href="#" className="estilos-a">Empanadas</a></li>
+                            <li><a href="#" className="estilos-a">Pizza</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div>
+                    <button className="botones-menu">Login</button>
+                </div>
+
+            </header>
+    )
+}
+
+export default Navbar
